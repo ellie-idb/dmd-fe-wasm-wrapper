64bb120565cfb978b6b4350d0caa826ab0c6967c
hatf0 <harrison@0xcc.pw>
2021-10-14 17:06:46 -0400

    Fix tabbing in file

--- source/app.d
+++ source/app.d
@@ -3,22 +3,22 @@ import dmd.frontend;
 
 void main(string[] args)
 {
-	import std.algorithm : each;
-	writeln("Initializing dmd...");
-	initDMD();
-	// findImportPaths.each!addImport;
-	auto t = parseModule("test.d", q{
+    import std.algorithm : each;
+    writeln("Initializing dmd...");
+    initDMD();
+    // findImportPaths.each!addImport;
+    auto t = parseModule("test.d", q{
         void foo()
         {
             foreach (i; 0..10) {}
         }
     });
 
-	assert(!t.diagnostics.hasErrors);
+    assert(!t.diagnostics.hasErrors);
     assert(!t.diagnostics.hasWarnings);
 
-	writeln("Running full semantic over file");
+    writeln("Running full semantic over file");
     t.module_.fullSemantic;
-	auto generated = t.module_.prettyPrint();
-	writefln("%s", generated);
+    auto generated = t.module_.prettyPrint();
+    writefln("%s", generated);
 }
